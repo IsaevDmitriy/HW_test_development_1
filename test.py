c4f9c6de0a3d3a3c7c01c798423beb069d5bff80
Dmitriy <ice988@mail.ru>
2021-03-25 13:02:54 +0500

    Сhange parametrize

--- test.py
+++ test.py
@@ -12,16 +12,16 @@ class TestSomething:
         expected = '\npassport 2207 876234 Василий Гупкин\ninvoice 11-2 Геннадий Покемонов\ninsurance 10006 Аристарх Павлов\n'
         assert show_all_docs_info() == expected
 
-    @pytest.mark.parametryze("test_input,expected", (['11-2', "Геннадий Покемонов"], ['10006', "Аристарх Павлов"], ['2207 876234', "Василий Гупкин"], [11-2, None]))
+    @pytest.mark.parametrize("test_input,expected", (['11-2', "Геннадий Покемонов"], ['10006', "Аристарх Павлов"], ['2207 876234', "Василий Гупкин"], [11-2, None]))
     def test_get_doc_owner_name(self, test_input, expected):
         assert get_doc_owner_name(test_input) == expected
 
 
-    @pytest.mark.parametryze("test_input,expected", (['11-2', True], ['10006', True], ['2207 876234', True], [11-2, None]))
+    @pytest.mark.parametrize("test_input,expected", (['11-2', True], ['10006', True], ['2207 876234', True], [11-2, False]))
     def test_check_document_existance(self, test_input, expected):
         assert check_document_existance(test_input) is expected
 
-    @pytest.mark.parametryze("test_input,expected", (['10006', '2'], ['11-2', '1'], ['2207 876234', '1'], [11-2, None]))
+    @pytest.mark.parametrize("test_input,expected", (['10006', '2'], ['11-2', '1'], ['2207 876234', '1'], [11-2, None]))
     def test_get_doc_shelf(self, test_input, expected):
         assert get_doc_shelf(test_input) == expected
 
@@ -29,11 +29,11 @@ class TestSomething:
         assert add_new_doc('10008', 'insurance', 'Семён Макаров', '3') == '3'
         assert add_new_doc('10008', 'insurance', 'Семён Макаров', 3) != '3'
 
-    @pytest.mark.parametryze("test_input,expected", (['2', False], ['5', True]))
+    @pytest.mark.parametrize("test_input,expected", (['2', False], ['5', True]))
     def test_add_new_shelf(self, test_input, expected):
         assert add_new_shelf(test_input) is expected
 
-    @pytest.mark.parametryze("test_input, expected1, expected2", (['10006', '10006', True], ['11-2', '11-2', True]))
-    def test_delete_doc(self, test_input, expected1, expected2):
-        assert delete_doc(test_input) == (expected1, expected2)
-        assert delete_doc(test_input) == (expected1, expected2)
+    @pytest.mark.parametrize("test_input, expected", (['10006', ('10006', True)], ['11-2', ('11-2', True)], [11-2, None]))
+    def test_delete_doc(self, test_input, expected):
+        assert delete_doc(test_input) == expected
+
